485e31f74389bc4a973f1b23753ee59b5f829c95
ShuaiHua Du <shuaihua.du@outlook.com>
2024-05-19 20:22:43 +0800

    Add html template.

--- DjangoWebProject1/HelloDjangoApp/views.py
+++ DjangoWebProject1/HelloDjangoApp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",
+        {
+            "title":"Hello Django",
+            "message":"Hello Django!",
+            "content": " on " + now.strftime("%A, %d %B, %Y at %X")
+        },
+    )
