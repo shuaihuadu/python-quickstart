e602fc76defb2ecc846c79bd36fb5b5f5081b876
ShuaiHua Du <shuaihua.du@outlook.com>
2024-11-29 11:56:06 +0800

    Add examples.

---
+++ src/PythonApplication1/test_format.py
@@ -0,0 +1,7 @@
+language = "zh"
+
+model_file_path = f"BM25_CORPUS_{language.upper()}_MODEL_FILE_PATH"
+
+print(f"BM25 Embedding language:{language}, use model file:{model_file_path}")
+
+print(__path__)
